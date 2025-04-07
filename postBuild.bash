c59331a3ec77cc71630af78b57049ca9ec0c2c00
nv-edwli <edwli@nvidia.com>
2025-04-07 21:15:54 +0000

    Project Spec Modified. Modified postBuild.bash
    - Updated start_command in app 'jupyterlab'
    - Modified files: postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -3,7 +3,7 @@ set -e
 
 # Install deps to run the API in a seperate venv to isolate different components
 conda create --name api-env -y python=3.10 pip
-$HOME/.conda/envs/api-env/bin/pip install fastapi==0.109.2 uvicorn[standard]==0.27.0.post1 python-multipart==0.0.7 langchain==0.0.335 langchain-community==0.0.19 openai==1.55.3 httpx==0.27.2 unstructured[all-docs]==0.12.4 sentence-transformers==2.7.0 llama-index==0.9.44 dataclass-wizard==0.22.3 pymilvus==2.3.1 opencv-python==4.8.0.76 hf_transfer==0.1.5 text_generation==0.6.1 transformers==4.40.0 nltk==3.8.1
+$HOME/.conda/envs/api-env/bin/pip install fastapi==0.109.2 uvicorn[standard]==0.27.0.post1 python-multipart==0.0.7 langchain==0.0.335 langchain-community==0.0.19 openai==1.55.3 httpx==0.27.2 unstructured[all-docs]==0.17.2 sentence-transformers==2.7.0 llama-index==0.9.44 dataclass-wizard==0.22.3 pymilvus==2.3.1 opencv-python==4.8.0.76 hf_transfer==0.1.5 text_generation==0.6.1 transformers==4.40.0 nltk==3.8.1
 
 # Install deps to run the UI in a seperate venv to isolate different components
 conda create --name ui-env -y python=3.10 pip
