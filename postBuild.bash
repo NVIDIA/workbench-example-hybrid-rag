980d11d0c24efdddcc9491883c2f499aced0e2e8
nv-edwli <edwli@nvidia.com>
2025-06-03 21:57:36 +0000

    Modified postBuild.bash
    - Modified files: postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -9,7 +9,7 @@ $HOME/.conda/envs/api-env/bin/pip install fastapi==0.109.2 uvicorn[standard]==0.
 conda create --name ui-env -y python=3.10 pip
 $HOME/.conda/envs/ui-env/bin/pip install dataclass_wizard==0.22.2 gradio==4.15.0 jinja2==3.1.2 numpy==1.25.2 protobuf==3.20.3 PyYAML==6.0 uvicorn==0.22.0 torch==2.1.1 tiktoken==0.7.0 regex==2024.5.15 fastapi==0.112.2
 
-sudo -E /opt/conda/bin/pip install anyio==4.3.0 pymilvus==2.3.1 transformers==4.40.0
+sudo -E /opt/conda/bin/pip install anyio==4.3.0 pymilvus==2.3.1 transformers==4.40.0 marshmallow==3.20.1
 
 sudo -E mkdir -p /mnt/milvus
 sudo -E mkdir -p /data
